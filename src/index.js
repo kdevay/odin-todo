140ce1b902907b17ec1b6036c11fcfe1c7ecdbf0
Katherine de Vay <katdevay2@gmail.com>
2022-10-31 12:29:42 -0500

    Add plugin for auto html generation based off src html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import './style.css';
 import './normalize.css';
+import Add from './add.png';
+import Bell from './bell.png';
+import Noti from './noti.png';
+
+const iconContainer = document.getElementById('icon-cont');
+// Add icons to div
+
+// const menuImage = document.createElement('img');// image
+// menuImage.setAttribute('id', 'mlogo');
+// menuImage.setAttribute('src', CafeLogo);
+// menuTab.appendChild(menuImage);
+
+const addIcon = document.createElement('img');
+addIcon.setAttribute('class', 'icon');
+addIcon.setAttribute('src', Add);
+iconContainer.appendChild(addIcon);
+
+const bellIcon = document.createElement('img');
+bellIcon.setAttribute('class', 'icon');
+bellIcon.setAttribute('src', Bell);
+iconContainer.appendChild(bellIcon);
+
+const notiIcon = document.createElement('img');
+notiIcon.setAttribute('class', 'icon');
+notiIcon.setAttribute('src', Noti);
+iconContainer.appendChild(notiIcon);
+
+counter++;
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+    ],
     output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -11,6 +17,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
